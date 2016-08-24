68fd90f7436e732da2b2e089df5220f09fddb92e
John Doe <xzzz9097@gmail.com>
2016-08-24 23:47:52 +0200

    Custom TextField to disable rich text import during copy The formatting is handled by InputFormatter

---
+++ Binary Calculator/InputTextField.h
@@ -0,0 +1,13 @@
+//
+//  InputTextView.h
+//  Binary Calculator
+//
+//  Created by Marco Albera on 24/08/16.
+//  Copyright © 2016 Edge Apps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface InputTextField : NSTextField
+
+@end
