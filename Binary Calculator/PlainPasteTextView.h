982755b74374ffba7cf2c3ed8c8a9002a196527c
John Doe <xzzz9097@gmail.com>
2016-09-12 16:35:06 +0200

    Paste plain text

---
+++ Binary Calculator/PlainPasteTextView.h
@@ -0,0 +1,13 @@
+//
+//  PlainPasteTextView.h
+//  Binary Calculator
+//
+//  Created by Marco Albera on 12/09/16.
+//  Copyright © 2016 Edge Apps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PlainPasteTextView : NSTextView
+
+@end
